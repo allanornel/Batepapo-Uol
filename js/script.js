4b0deae807c94b385bb0540d440e8d3ccdaaad33
allanornel <allanornel@gmail.com>
2022-02-10 19:50:41 -0300

    Tentativa com erro da parte da Variavel primeirasMensagens

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let nameUser;
+let preencheuPrimeirasMensagens = false;
 cadastrarName();
 
 function cadastrarName() {
@@ -27,17 +28,26 @@ function manterConexao() {
     requisicaoConectado.catch(tratarErro);
 }
 setInterval(manterConexao, 5 * 1000);
-buscarMensagens();
+setInterval(buscarMensagens, 3 * 1000);
 
 function buscarMensagens() {
     let requisicaoMensagens = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-    requisicaoMensagens.then(processarMensagens);
+    if (!preencheuPrimeirasMensagens) {
+        requisicaoMensagens.then(processarMensagens);
+    } else {
+        requisicaoMensagens.then(atualizarMensagens);
+    }
     requisicaoMensagens.catch(tratarErro);
 }
 
 function processarMensagens(mensagens) {
-    console.log(mensagens);
+    if (!preencheuPrimeirasMensagens) {
+        let primeirasMensagens = mensagens.data;
+    } else {
+        primeirasMensagens = mensagens;
+    }
     let chat = document.querySelector('main');
+    preencheuPrimeirasMensagens = true;
     for (let index = 0; index < mensagens.data.length; index++) {
         let from = mensagens.data[index].from;
         let to = mensagens.data[index].to;
@@ -56,9 +66,15 @@ function processarMensagens(mensagens) {
     element.scrollIntoView();
 }
 
-function enviarMensagem (){
-    
+function atualizarMensagens(mensagensNovas) {
+    let difference = mensagensNovas.data.filter(x => !primeirasMensagens.includes(x));
+    if (difference != []) {
+        processarMensagens(difference);
+    }
+}
 
-    let mensagem = {from: `${nameUser}`, to: 'Todos', text: `${message}`, type: 'message'}
+
+function enviarMensagem() {
+    let mensagem = { from: `${nameUser}`, to: 'Todos', text: `${message}`, type: 'message' }
     let envioMensagem = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', mensagem);
 }
